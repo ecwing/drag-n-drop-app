14032cdca2db52d0545c838244a8cfea6f93f6c1
ecwing <ericcwing@gmail.com>
2019-07-29 23:55:37 -0400

    stlying shadows when dragged items

--- main.js
+++ main.js
@@ -30,10 +30,15 @@ for (let i =0; i < list_items.length; i++) {
         });
         list.addEventListener('dragenter', function(e) {
             e.preventDefault();
+            this.style.backgroundColor = 'rgba(0,0,0,0.2)';
         });
+        list.addEventListener('dragleave', function(e) {
+            this.style.backgroundColor = 'rgba(0,0,0,0.1)';
+        })
         list.addEventListener('drop', function(e) {
             console.log('drop');
             this.append(draggedItem);
+            this.style.backgroundColor = 'rgba(0,0,0,0.1)';
         })
     }
 }
